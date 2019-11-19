68990af1b32ab3486c3871b775ff58d996fedfcc
Lee Weiquan <franklee809@gmail.com>
2019-11-19 08:44:51 +0800

    first commit

---
+++ Movie.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const db = 'mongodb+srv://test1:123123qwe@cluster0-kzfrl.mongodb.net/MovieDb?retryWrites=true&w=majority';
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(error => {
+    console.log('Mongoose connetion error: ', error);
+  });
+
+const schema = mongoose.Schema({
+  title: { type: String },
+  year: { type: String },
+  genre: { type: String },
+  actors: { type: String },
+  plot: { type: String },
+  poster: { type: String }
+});
+
+const Movie = mongoose.model('Movie', schema, 'movieCollection');
+
+module.exports = Movie;
